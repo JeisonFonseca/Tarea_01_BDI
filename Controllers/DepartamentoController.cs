e1d0d119693ff655d61a3e4930e5d36dcb74efd7
FERNANDO FONSECA BRENES JEISON <jeison.fb01@estudiantec.cr>
2022-08-30 23:03:17 -0600

    actualizado

---
+++ Controllers/DepartamentoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tarea_1.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/SoftwareController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tarea_1.Models;
+
+namespace Tarea_1.Controllers
+{
+    public class SoftwareController : Controller
+    {
+        private readonly Tarea_1Context _context;
+
+        public SoftwareController(Tarea_1Context context)
+        {
+            _context = context;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            return View(await _context.Softwares.ToListAsync());
+        }
+    }
+}
